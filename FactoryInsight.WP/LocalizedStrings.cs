3669d81471aa45bdd1c1ab9ebd2fa0a9725b78c7
mallibone <sirkenix@gmail.com>
2014-04-03 08:44:34 +0200

    Initial commit

---
+++ FactoryInsight.WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using FactoryInsight.WP.Resources;
+
+namespace FactoryInsight.WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
